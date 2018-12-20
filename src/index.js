199ab0b8cbe8d2d1319fb1bb8d9d38b8a1346b35
Lexi Morgan <leximorgan816@gmail.com>
2018-12-20 16:58:44 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -4,17 +4,57 @@
 import sayHello from './hello';
 sayHello('World');
 
+
+//jQuery import:
+const $ = require("jquery");
+// import $ from ("jquery");
+
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
+  $("#loading").hide();
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
+    $("#movie-header").html("Movies");
+    $("#movie-list").append(`<li>id#${id} - ${title} - rating: ${rating}</li>`);
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+
+//import our javascript here:
+
+
+
+
+
+
+//loading animation js----------------------------------/
+ $(document).ready(function(){
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");
+});
+//END loading animation js------------------------------/
+
+
+
+
+
+
+// //our javascript here:
+// $(window).load(function(){
+//     $("#loading").html("LOADING...")
+//     $("body").css("background-color", "blue");
+// });
+//
+//
+// $(document).ready(function(){
+//     $("#loading").html("Movies")
+// });
+
