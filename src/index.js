017ecb37439f29b8621cded784ee21c36821d95f
Sarah Barron <s.barron2338@gmail.com>
2018-12-21 11:58:18 -0600

    added and updated getting star rating value

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ getMovies().then((movies) => {
     $("#movie-header").html("Movies");
     $("#movie-list").append(`<li>id#${id} - ${title} - rating: ${rating}</li>`);
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $(".hidden-on-load").css("display", "inline-block");
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
@@ -42,19 +43,38 @@ getMovies().then((movies) => {
 });
 //END loading animation js------------------------------/
 
+//API Post for adding Movies ---------------------------/
+
+$(':radio').click(function() {
+    console.log (this.value);
+    $('#rating').val(this.value);
+  });
+$('#add-movie-btn').click(function () {
+  console.log('hello!');
+let movieTitle = $('#movie-title').val();
+let rating = $('#rating').val();
+  console.log(movieTitle);
+  console.log(rating);
+
+const newMovie = {title: movieTitle, rating: rating};
+console.log(newMovie);
+const url = '/api/movies';
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(newMovie),
+};
+fetch(url, options)
+  .then('New movie added!')
+  // .catch(console.log(error(e)));
+})
+
+//End of api for adding movies------------------------/
 
 
 
 
 
-// //our javascript here:
-// $(window).load(function(){
-//     $("#loading").html("LOADING...")
-//     $("body").css("background-color", "blue");
-// });
-//
-//
-// $(document).ready(function(){
-//     $("#loading").html("Movies")
-// });
 
